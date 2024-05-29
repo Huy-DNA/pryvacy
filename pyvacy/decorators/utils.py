c74e82ca494043959f74d9592c6ce6f2b57839b9
Huy-DNA <huydo862003@gmail.com>
2024-05-29 20:06:15 +0700

    fix: allow Type in get/set access policy

--- pyvacy/decorators/utils.py
+++ pyvacy/decorators/utils.py
@@ -1,12 +1,12 @@
-from typing import Callable
+from typing import Callable, Type, Union
 
 from pyvacy.access_policy import AccessPolicy
 
 
-def get_access_policy(fn: Callable) -> AccessPolicy:
-    return fn.__dict__.get("@@_access_control", AccessPolicy.PUBLIC)
+def get_access_policy(obj: Union[Callable, Type]) -> AccessPolicy:
+    return obj.__dict__.get("@@_access_control", AccessPolicy.PUBLIC)
 
-def set_access_policy(fn: Callable, access_policy: AccessPolicy):
-    fn.__dict__["@@_access_control"] = access_policy
+def set_access_policy(obj: Union[Callable, Type], access_policy: AccessPolicy):
+    setattr(obj, "@@_access_control", access_policy)
 
 
