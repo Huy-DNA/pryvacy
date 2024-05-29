9d61ff0abfd8732d105cb0a8dc921c67328dc4b6
Huy-DNA <huydo862003@gmail.com>
2024-05-29 20:09:08 +0700

    chore: scaffold nested_classes

--- pyvacy/decorators/pyvacy_decorator/__init__.py
+++ pyvacy/decorators/pyvacy_decorator/__init__.py
@@ -1,6 +1,7 @@
 from typing import Type, TypeVar
 
 from pyvacy.decorators.pyvacy_decorator import instance_methods
+from pyvacy.decorators.pyvacy_decorator import nested_classes
 
 
 T = TypeVar('T')
@@ -10,7 +11,8 @@ def pyvacy(cls: Type[T]) -> Type[T]:
     setattr(cls, "@@_pyvacified", ())
 
     instance_methods.init(cls)
-
+    nested_classes.init(cls)
+    
     old_init_subclass = cls.__init_subclass__
     @classmethod
     def init_subclass_wrapper(cls, **kwargs):
---
+++ pyvacy/decorators/pyvacy_decorator/nested_classes.py
@@ -0,0 +1,18 @@
+import inspect
+from typing import Type
+
+from pyvacy.access_policy import AccessPolicy
+from pyvacy.decorators.utils import get_access_policy 
+
+
+def init(cls: Type):
+    nested_classes = { name: obj for name, obj in inspect.getmembers(cls, inspect.isclass) if obj.__module__ == cls.__module__ }
+
+    for name, nested_cls in nested_classes.items():
+        match get_access_policy(nested_cls):
+            case AccessPolicy.PUBLIC:
+                pass
+            case AccessPolicy.PRIVATE:
+                pass
+            case AccessPolicy.PROTECTED:
+                pass
